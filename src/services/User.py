e818ffcdcee748ae2275ec17a2c146862b50fb92
Aurelio Gonzalez <agonzalez@sisu.mx>
2022-05-24 12:23:05 -0500

    Modifico documento usuario id automatica

--- src/services/User.py
+++ src/services/User.py
@@ -1,7 +1,7 @@
 from config.firebaseAdminConfig import db
 
 def createUser(userData):
-    doc_ref = db.collection('users').document(userData.get("id"))
+    doc_ref = db.collection('users').document()
     doc_ref.set({
         'id': userData.get("id"),
         'username': userData.get("username"),
